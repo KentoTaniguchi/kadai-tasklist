b64c00e8780e4225079f0025cf5a93d8e67a2895
kento.taniguchi <kentosoccer315@gmail.com>
2020-06-20 08:11:23 +0000

    kadai-tasklist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
   end
 
@@ -8,7 +9,6 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-    
   end
 
   def create
