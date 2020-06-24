c4c290f166fb2788a752b8f0b9dcbcfe3c528a66
kento.taniguchi <kentosoccer315@gmail.com>
2020-06-24 14:46:27 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  before_action :correct_user, only: [:destroy, :edit, :update, :show]
+  before_action :require_user_logged_in
+  before_action :correct_user
   
   def index
+       @tasks = Task.all
        @task = current_user.tasks.build  
-       @tasks = current_user.tasks.order(id: :desc)
+     
   end
   def show
       
